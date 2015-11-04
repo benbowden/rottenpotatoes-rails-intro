147b99e399087dcb6655b3d26c301985cacae67f
benbowden <bbowden9@uncc.edu>
2015-11-04 21:04:07 +0000

    testing

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,6 +1,9 @@
 class MoviesController < ApplicationController
-  helper_method :hilight
-  helper_method :chosen_rating?
+  
+
+  def movie_params
+    params.require(:movie).permit(:title, :rating, :description, :release_date)
+  end
 
   def show
     id = params[:id] # retrieve movie ID from URI route
@@ -9,31 +12,49 @@ class MoviesController < ApplicationController
   end
 
   def index
+    
     @all_ratings = ['G','PG','PG-13','R']
-    session[:ratings] = params[:ratings] unless params[:ratings].nil?
-    session[:order] = params[:order] unless params[:order].nil?
+    
+    @byTitle = false
+    @titleHighlight = ""
+    @releaseDateHighlight = ""
+    
+    if params[:id] == "title_header"
+      @movies = Movie.order('movies.title asc')
+    
+      @titleHighlight = "hilite"
+      @releaseDateHighlight = ""
+        
+    elsif params[:id] == "release_date_header"
+      @movies = Movie.order('movies.release_date asc')
+      
+      @titleHighlight = ""
+      @releaseDateHighlight = "hilite"
+    
+    elsif params[:ratings].present?
+      @movies = Movie.where(rating: params[:ratings].keys)
+      
 
-    if (params[:ratings].nil? && !session[:ratings].nil?) || (params[:order].nil? && !session[:order].nil?)
-      redirect_to movies_path("ratings" => session[:ratings], "order" => session[:order])
-    elsif !params[:ratings].nil? || !params[:order].nil?
-      if !params[:ratings].nil?
-        array_ratings = params[:ratings].keys
-        return @movies = Movie.where(rating: array_ratings).order(session[:order])
-      else
-        return @movies = Movie.all.order(session[:order])
-      end
-    elsif !session[:ratings].nil? || !session[:order].nil?
-      redirect_to movies_path("ratings" => session[:ratings], "order" => session[:order])
+    
+      
     else
-      return @movies = Movie.all
+      @movies = Movie.all
+      
     end
+    
+
+  
+    
+     
+
   end
 
   def new
+    # default: render 'new' template
   end
 
   def create
-    @movie = Movie.create!(params[:movie])
+    @movie = Movie.create!(movie_params)
     flash[:notice] = "#{@movie.title} was successfully created."
     redirect_to movies_path
   end
@@ -44,7 +65,7 @@ class MoviesController < ApplicationController
 
   def update
     @movie = Movie.find params[:id]
-    @movie.update_attributes!(params[:movie])
+    @movie.update_attributes!(movie_params)
     flash[:notice] = "#{@movie.title} was successfully updated."
     redirect_to movie_path(@movie)
   end
@@ -55,18 +76,7 @@ class MoviesController < ApplicationController
     flash[:notice] = "Movie '#{@movie.title}' deleted."
     redirect_to movies_path
   end
+  
 
-  def hilight(column)
-    if(session[:order].to_s == column)
-      return 'hilite'
-    else
-      return nil
-    end
-  end
 
-  def chosen_rating?(rating)
-    chosen_ratings = session[:ratings]
-    return true if chosen_ratings.nil?
-    chosen_ratings.include? rating
-  end
 end
